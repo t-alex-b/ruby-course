0f67893599a85bd39a2c7a4acdc9bb87e372065f
t-alex-b <sunnoperatic@aol.com>
2021-05-13 22:32:56 -0600

    elsif and downcase

--- montys-color.rb
+++ montys-color.rb
@@ -1,19 +1,21 @@
 # Lesson 3: Prompt for Favorite Color
 
 puts "What is your favorite color?"
-color = gets.chop
-puts "Wow! My favorite color is #{color} also!"
+color = gets.downcase.chop
+#puts "Wow! My favorite color is #{color} also!"
 
 # Lesson 4: Using an If Statement
 
-if color == "blue"
-    puts "Wow! My favorite color is #{color} also!"
-end
+#if color == "blue"
+#    puts "Wow! My favorite color is #{color} also!"
+#end
 
 # Lesson 5: Adding an Else Statement
 
 if color == "blue"
     puts "Wow! My favorite color is #{color} also!"
+elsif color == "red"
+    puts "It's not as good as blue, but red is okay"
 else
     puts "I'm not a big fan of #{color}."
 end
